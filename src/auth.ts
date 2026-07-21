a6892a8ef307e4ebe1c4633ebf42ce7e5c1c54af
Raja <jraja0132@gmail.com>
2026-07-21 12:35:47 +0530

    change auth configure file for deployment

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,7 @@ import Google from "next-auth/providers/google";
 import { getBalance,createUser } from "./lib/ledger";
 
 export const {handlers,signIn,signOut,auth}=NextAuth({
+    trustHost : true,
     providers:[Google],
     callbacks:{
         async signIn({user}) {
